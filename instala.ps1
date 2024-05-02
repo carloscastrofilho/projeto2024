9a8ea84b87d0b5ff3f17a68309b9a17ac26f9aa4
carloscastrofilho <cpcftq@gmail.com>
2024-05-02 11:18:22 -0300

    ola

---
+++ instala.ps1
@@ -0,0 +1,9 @@
+npm install --prefix C:\ProgramData\npm -g @angular/cli
+
+GetEnvironmentVariable('PATH','machine') > C:\backup-path.txt
+
+$INCLUDE = "C:\tmp"
+
+SetEnvironmentVariable("NG", "valor", "Machine")
+
+SetEnvironmentVariable("VARIAVEL", "valor", "Machine")
